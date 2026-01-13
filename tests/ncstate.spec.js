d43f78bd4377ffb0b8b76978fd603a74b61e24a3
avelmuruganofficials-sudo <a.velmuruganofficials@gmail.com>
2026-01-13 18:19:50 +0530

    Add Jenkinsfile for Playwright CI

--- tests/ncstate.spec.js
+++ tests/ncstate.spec.js
@@ -8,7 +8,7 @@ test('Excel data based automation', async ({ page }) => {
   await page.goto('https://www.landydev.com/#/auth/login');
   await page.waitForLoadState('networkidle');
   await page.getByRole('textbox', { name: 'Email' }).fill('velmurugan@stepladdersolutions.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('Step@123');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Test@123');
   await page.getByRole('button', { name: 'Login' }).click();
   for (let i = 0; i < data.length; i++) {
     const row = data[i];
