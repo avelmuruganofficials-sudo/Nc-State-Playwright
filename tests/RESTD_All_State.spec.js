d60f0f5e5bcdf9b101de968fa7219dedeeb5ba7d
avelmuruganofficials-sudo <a.velmuruganofficials@gmail.com>
2026-02-10 10:14:38 +0530

    first commit

--- tests/RESTD_All_State.spec.js
+++ tests/RESTD_All_State.spec.js
@@ -10,7 +10,7 @@ test('Excel data based automation', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Password' }).fill('Test@123');
     await page.getByRole('button', { name: 'Login' }).click();
 
-    for (let i = 18; i < data.length; i++) {
+    for (let i = 19; i < data.length; i++) {
         const row = data[i];
         console.log(`Starting row ${i + 1} RiskId: ${row.RiskId}`);
         try {
